0664e0c13b0abcc1303167aba6b8f14887e9d40c
unknown <zach-morris@lambdastudents.com>
2021-08-31 21:06:49 -0400

    Test, eperm?

--- api/server.js
+++ api/server.js
@@ -1,2 +1,30 @@
 // implement your server here
 // require your posts router and connect it here
+
+//Imports
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+
+//Instance Of Express App
+const server = express();
+
+
+//Global Middlewarw
+server.use(express.json());
+
+
+//Consuming
+server.use('/api/posts', postsRouter);
+
+
+//Endpoints; Catchall
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'Sorry, Not Found'
+    })
+});
+
+
+//Exports; Exposing
+module.exports = server;
